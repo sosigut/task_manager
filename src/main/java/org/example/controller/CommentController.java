7ee4e966df42167b93497a4aa79391e09afda437
Sosigut <sosigut@gmail.com>
2026-01-29 19:28:48 +0300

    Comment Controller

---
+++ src/main/java/org/example/controller/CommentController.java
@@ -0,0 +1,30 @@
+package org.example.controller;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.example.dto.CommentResponseDto;
+import org.example.dto.CreateCommentRequestDto;
+import org.example.service.CommentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/tasks")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping("/{taskId}/comments")
+    public CommentResponseDto createComment(
+            @Valid @RequestParam CreateCommentRequestDto dto,
+            @PathVariable Long taskId) {
+        return commentService.createComment(taskId, dto);
+    }
+
+    @GetMapping("/{taskId}/comments")
+    public List<CommentResponseDto> getComments(@PathVariable Long taskId) {
+        return commentService.getComments(taskId);
+    }
+}
